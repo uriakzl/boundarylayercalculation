883e3b89e320d3d68bb4fe6d119adb186f7821f2
uriak <kairutheliu@gmail.com>
2025-07-17 11:14:34 -0400

    first commit

---
+++ boundarylayer.py
@@ -0,0 +1,15 @@
+#simple code to solve for boundary layer 
+#no inputs, keep it as simple as possible
+
+#mew is the viscosity of water in SI units
+
+#rho is the density of water in SI units
+
+#u is the liquid velocity
+
+#dH is the hydraulic diameter
+
+#calculate reynolds number, ensure it is below 2000 to ensure laminar flow
+
+#calculate follow theoreotical model. 
+
